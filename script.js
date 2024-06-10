89d8bfa3042c9998d385ecf3a0b02ee1930b7268
Ultr4viol3t <106609617+ultr4viol3t@users.noreply.github.com>
2024-06-10 08:42:40 +0200

    Finished etch a sketch and reset functionality.

---
+++ script.js
@@ -0,0 +1,47 @@
+// Query selectors
+
+
+
+// Create a function to generate the grid
+// Create two nested for loops one for rows and one for cloumns
+// Within the loops create new divs and give them an id
+// Append all the mainContainer
+function createGrid(size) {
+    const mainContainer = document.querySelector(".mainContainer"); 
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        //row.addEventListener("mouseover", changeColor);
+        mainContainer.appendChild(row);
+        for (let i = 0; i < size; i++) {
+            const col = document.createElement("div");
+            col.setAttribute("class", "col");
+            col.addEventListener("mouseover", changeColor);
+            row.appendChild(col);
+        }
+    };
+
+};
+
+function changeColor(event) {
+    event.target.style.backgroundColor = "tomato";
+
+};
+
+const resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener("click", resetGrid);
+
+function resetGrid(){
+    const mainContainer = document.querySelector(".mainContainer");
+    mainContainer.innerHTML = "";
+    let gridSize = prompt("How big is your grid going to be?");
+    if (gridSize > 100) {
+        alert("Smaller than 100");
+    } else {
+        createGrid(Number(gridSize));
+    }
+    
+};
+
+
+createGrid(16);
