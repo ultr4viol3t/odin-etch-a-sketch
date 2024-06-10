4584e096bc2c6ad85e0404335b71a71d299f7a9f
Ultr4viol3t <106609617+ultr4viol3t@users.noreply.github.com>
2024-06-10 08:51:48 +0200

    Added color randomizer.

--- script.js
+++ script.js
@@ -24,8 +24,18 @@ function createGrid(size) {
 };
 
 function changeColor(event) {
-    event.target.style.backgroundColor = "tomato";
+    event.target.style.backgroundColor = getRandomColor();
 
+};
+
+// Randomize color
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 };
 
 const resetButton = document.querySelector("#resetButton");
